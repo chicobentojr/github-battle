7230e033dab2b0769ce80ce158ece42c9a5b8f55
Francisco Bento <francisco.bento.jr@hotmail.com>
2018-02-27 09:26:14 -0300

    Update UserDetails UI

--- app/components/UserDetails.js
+++ app/components/UserDetails.js
@@ -1,19 +1,33 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
+function UserItem(props) {
+  return (
+    <li className='list-group-item'>{props.children}</li>
+  )
+}
+
+
 function UserDetails(user){
   return (
     <div>
-      {!!user.score && <li className='list-group-item'><h3>Score: {user.score}</h3></li>}
-      {<li className='list-group-item'><img src={user.info.avatar_url} className='img-rounded img-responsive' /></li>}
-      {user.info.name && <li className='list-group-item'>Name: {user.info.name}</li>}
-      {<li className='list-group-item'>Username: {user.info.login}</li>}
-      {user.info.location && <li className='list-group-item'>Location: {user.info.location}</li>}
-      {user.info.company && <li className='list-group-item'>Company: {user.info.company}</li>}
-      {<li className='list-group-item'>Followers: {user.info.followers}</li>}
-      {<li className='list-group-item'>Following: {user.info.following}</li>}
-      {<li className='list-group-item'>Public Repos: {user.info.public_repos}</li>}
-      {user.info.blog && <li className='list-group-item'>Name: {user.info.blog}</li>}
+      {!!user.score && <UserItem><h3>Score: {user.score}</h3></UserItem>}
+      <UserItem>
+        <a target='_blank' href={user.info.html_url}>
+          <img
+            style={{display:'block', margin: '0 auto'}}
+            src={user.info.avatar_url}
+            className='img-rounded img-responsive' />
+        </a>
+      </UserItem>
+      {user.info.name && <UserItem>Name: {user.info.name}</UserItem>}
+      <UserItem>Username: {user.info.login}</UserItem>
+      {user.info.location && <UserItem>Location: {user.info.location}</UserItem>}
+      {user.info.company && <UserItem>Company: {user.info.company}</UserItem>}
+      <UserItem>Followers: {user.info.followers}</UserItem>
+      <UserItem>Following: {user.info.following}</UserItem>
+      <UserItem>Public Repos: {user.info.public_repos}</UserItem>
+      {user.info.blog && <UserItem>Blog: <a target='_blank' href={user.info.blog}> {user.info.blog}</a></UserItem>}
     </div>
   )
 }
@@ -30,6 +44,7 @@ UserDetails.propTypes = {
     login: PropTypes.string.isRequired,
     name: PropTypes.string,
     public_repos: PropTypes.number.isRequired,
+    html_url: PropTypes.string.isRequired,
   })
 }
 
